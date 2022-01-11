6561b3763a893c3b0974e171f32f82a7e5abc95b
ibender90 <stalev.artjom@gmail.com>
2022-01-11 11:07:42 +0200

    webapp uploaded to repository

---
+++ src/main/java/code/models/Person.java
@@ -0,0 +1,64 @@
+package code.models;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "enter name")
+    @Size(min = 2, message = "minimal size is 2 letters")
+    private String name;
+
+    @Min(value = 0, message = "minimal age is 0")
+    private int age;
+
+    @NotEmpty(message = "enter email")
+    @Email(message = "incorrect email")
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
